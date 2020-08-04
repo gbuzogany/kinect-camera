58324733bd7461e43e98fd99cea4fda6f92c7939
Gustavo Buzogany Eboli <gbuzogany@gmail.com>
2020-08-04 16:32:02 +0200

    Initial Commit

---
+++ KinectVideo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KinectVideo
+//
+//  Created by Gustavo Buzogany Eboli on 04/08/2020.
+//  Copyright © 2020 Gustavo Buzogany Eboli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
